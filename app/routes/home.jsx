483730dcff5289345bcf98df83b8cb17b5e45281
SayRyan <richardryancarvalho.infotarde@gmail.com>
2026-02-23 21:01:47 -0300

    teste deploy

--- app/routes/home.jsx
+++ app/routes/home.jsx
@@ -2,7 +2,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}) {
   return [
-    { title: "New React Router App" },
+    { title: "TESTE DO RYAN X MATHEUS" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
